e8302f13f72180bf4dedca3a32348082f09f2580
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-06-22 21:00:45 -0400

    Updated for 1.1.3

--- src/TankLock/deploy.bat
+++ src/TankLock/deploy.bat
@@ -1,11 +1,27 @@
-﻿mkdir R:\KSP_1.0.5_Dev\GameData\TankLock
-mkdir R:\KSP_1.0.5_Dev\GameData\TankLock\Plugin
+﻿set H=R:\KSP_1.1.2_dev
+echo %H%
+
+set d=%H%
+if exist %d% goto one
+mkdir %d%
+:one
+set d=%H%\Gamedata
+if exist %d% goto two
+mkdir %d%
+:two
+set d=%H%\Gamedata\TankLock
+if exist %d% goto three
+mkdir %d%
+:three
+set d=%H%\Gamedata\TankLock\Plugin
+if exist %d% goto four
+mkdir %d%
+:four
 
 
+copy /Y "%~dp0bin\Debug\TankLock.dll" "%H%\GameData\TankLock\Plugin"
+copy /Y "%~dp0TankLock.version" "%H%\GameData\TankLock"
+copy /Y "%~dp0TankLock.cfg" "%H%\GameData\TankLock"
 
-copy /Y "%~dp0bin\Debug\TankLock.dll" "R:\KSP_1.0.5_Dev\GameData\TankLock\Plugin"
-copy /Y "%~dp0TankLock.version" "R:\KSP_1.0.5_Dev\GameData\TankLock"
-copy /Y "%~dp0TankLock.cfg" "R:\KSP_1.0.5_Dev\GameData\TankLock"
-
-copy /Y "%~dp0b..\License.txt" "R:\KSP_1.0.5_Dev\GameData\TankLock"
-copy /Y "%~dp0..\..\README.md" "R:\KSP_1.0.5_Dev\GameData\TankLock"
+copy /Y "%~dp0b..\License.txt" "%H%\GameData\TankLock"
+copy /Y "%~dp0..\..\README.md" "%H%\GameData\TankLock"
